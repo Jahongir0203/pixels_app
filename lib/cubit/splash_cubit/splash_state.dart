9c4d190b303b05289cf9e815255ceeaa0eda37ab
Jahongir0203 <you@example.com>
2023-12-10 12:07:48 +0300

    splash_bloc done

--- lib/cubit/splash_cubit/splash_state.dart
+++ lib/cubit/splash_cubit/splash_state.dart
@@ -1,17 +1,17 @@
-part of 'splash_cubit.dart';
-
-@immutable
-abstract class SplashState {}
-class SplashInitial extends SplashState{}
-class SplashLoaded extends SplashState{}
-// class SplashState {
-//   final PhotoModel? photoModel;
+// part of 'splash_cubit.dart';
 //
-//   SplashState({this.photoModel});
-//
-//   SplashState copyWith({
-//     PhotoModel? photoModel,
-//   }) {
-//     return SplashState(photoModel: photoModel ?? this.photoModel);
-//   }
-// }
+// @immutable
+// abstract class SplashState {}
+// class SplashInitial extends SplashState{}
+// class SplashLoaded extends SplashState{}
+// // class SplashState {
+// //   final PhotoModel? photoModel;
+// //
+// //   SplashState({this.photoModel});
+// //
+// //   SplashState copyWith({
+// //     PhotoModel? photoModel,
+// //   }) {
+// //     return SplashState(photoModel: photoModel ?? this.photoModel);
+// //   }
+// // }
--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:pixels_app/cubit/splash_cubit/splash_cubit.dart';
+import 'package:pixels_app/bloc/splash_bloc/splash_bloc.dart';
 import 'package:pixels_app/utils/app_Png.dart';
 
 class SplashPage extends StatelessWidget {
   SplashPage({Key? key}) : super(key: key);
-  SplashCubit splashCubit = SplashCubit();
+  SplashBloc splashBloc = SplashBloc();
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => splashCubit,
-      child: BlocListener<SplashCubit, SplashState>(
-        bloc: splashCubit.getNavigation(),
+      create: (context) => splashBloc,
+      child: BlocListener<SplashBloc, SplashState>(
+        bloc: splashBloc..add(SplashEvent()),
         listener: (context, state) {
-          if (state is SplashLoaded) {
-            Navigator.pushNamed(context, '/home');
+          if (state is SplashCompleted) {
+            Navigator.pushReplacementNamed(context, '/home');
           }
         },
         child: Scaffold(
